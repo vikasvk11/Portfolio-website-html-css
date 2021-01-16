b8e04ed86ea66915a63d6971fd4b9e1f6cd4f6a9
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-01-16 23:24:53 +0530

    feat: darkmode
    Implemented dark mode with localstorage, so that the page remebers the preference.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var btn = document.querySelector('.nav-brand');
 
 function clickHandler() {
@@ -5,3 +6,34 @@ function clickHandler() {
 }
 
 btn.addEventListener("click", clickHandler);
+*/
+
+let darkModevar = localStorage.getItem("darkMode");
+const btn = document.querySelector(".nav-brand");
+
+console.log(darkModevar);
+
+function enableDarkMode() {
+    document.body.classList.add('dark');
+    localStorage.setItem("darkMode", "enabled");
+}
+
+function disableDarkMode() {
+    document.body.classList.remove('dark');
+    localStorage.setItem("darkMode", null);
+}
+
+if(darkModevar === 'enabled') {
+    enableDarkMode();
+}
+
+btn.addEventListener('click', () => {
+    darkModevar = localStorage.getItem("darkMode");
+    if(darkModevar !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+    console.log(darkModevar);
+})
+
