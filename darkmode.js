0781d06cfb16ddf1014f241d3b31cb2174d8127c
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-01-18 13:11:58 +0530

    fix: dark mode flash
    Used two separate csss files for light and dark

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,17 @@
 let darkModevar1 = localStorage.getItem("darkMode");
+let style1 = document.getElementById('mystylesheet');
+
+function enableDarkMode() {
+    style1.href = 'dark.css'
+    localStorage.setItem("darkMode", "enabled");
+  
+}
+
+function disableDarkMode() {
+    style1.href = 'styles.css'
+    localStorage.setItem("darkMode", null);
+}
 
 if(darkModevar1 === 'enabled') {
-    console.log(document.body.classList);
-    document.body.classList.add('dark');
+    enableDarkMode();
 }
---
+++ darkmode2.js
@@ -0,0 +1,29 @@
+let darkModevar = localStorage.getItem("darkMode");
+let style = document.getElementById('mystylesheet');
+const btn = document.querySelector(".nav-brand");
+
+
+function enableDarkMode() {
+    style.href = 'dark.css'
+    localStorage.setItem("darkMode", "enabled");
+}
+
+function disableDarkMode() {
+    style.href = 'styles.css'
+    localStorage.setItem("darkMode", null);
+}
+
+if(darkModevar === 'enabled') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+btn.addEventListener('click', () => {
+    darkModevar = localStorage.getItem("darkMode");
+    if(darkModevar !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
